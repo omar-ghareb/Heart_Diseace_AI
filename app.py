5e29b686005d7ae63c79579e61e15c3ab874bcca
omar-ghareb <omrgharebb@gmail.com>
2026-08-01 18:50:03 +0300

    Refactor app.py to remove duplicate imports and code

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 with st.sidebar:
     # استخدام use_container_width ليأخذ اللوجو العرض المناسب بشكل أنيق
-    st.image("Heart_Disease_AI/assets/logo.PNG.jpeg", use_container_width=True)
+    st.image("assets/logo.PNG.jpeg", use_container_width=True)
     
     st.title("Heart Disease AI")
     
